e46fa331f41f3962a0ecf6b8caf630c57feddda7
JesseX <shallow.wow@gmail.com>
2022-08-17 23:38:34 +0800

    add http import plugin

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
 // src/index.jsx
-import Server from "react-dom/server"
+// import Server from "react-dom/server"
 
-let Greet = () => <h1>Hello Esbuild</h1>;
-console.log(Server.renderToString(<Greet />));
+// let Greet = () => <h1>Hello Esbuild</h1>;
+// console.log(Server.renderToString(<Greet />));
+
+import { render } from "https://cdn.skypack.dev/react-dom"
+import React from "https://cdn.skypack.dev/react"
+
+const Greet = () => <h1>Hello Esbuild</h1>;
+
+render(<Greet />, document.getElementById("root"));
